5fcf5e3b041ae15aa25ce4af2ab07fd0385f9e40
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2013-01-07 21:24:52 -0500

    bump python fiddle version

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,7 @@
 def site(context):
     return {
             'favicon': 'python_cloud.png',
-            'application': 'Python Fiddle'
+            'application': 'Python Fiddle',
+            'searchid': '001890374624298101749:vgz9z7-mrsu'
         }
 
