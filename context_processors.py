cc7335238516b6bf828476f3b258b17b44a7c749
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2013-08-10 10:39:41 -0400

    add context processor for displaying languages in navigation area

--- context_processors.py
+++ context_processors.py
@@ -2,6 +2,7 @@ def site(context):
     return {
             'favicon': 'python_cloud.png',
             'application': 'Python Fiddle',
-            'searchid': '001890374624298101749:vgz9z7-mrsu'
+            'searchid': '001890374624298101749:vgz9z7-mrsu',
+            'multiple_languages': False
         }
 
