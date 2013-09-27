89023fcfcf46788aa657125a4deae5cca86400e9
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2013-09-27 15:02:51 -0400

    update static root path to be relative

--- settings.default.py
+++ settings.default.py
@@ -58,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join("/fiddlesalad/static")
+STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_DIR), "fiddlesalad\\static")
 
 PYTHON_LIB_DIR = os.path.join(PROJECT_DIR, "static\\lib")
 
